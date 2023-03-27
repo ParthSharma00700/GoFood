aa0c53e6924772b915bcb7a0310e0e43764a6b7a
tarundhiman85 <tarundhiman0047@gmail.com>
2023-03-27 13:08:59 +0530

    added search and fixed import

--- src/App.js
+++ src/App.js
@@ -7,10 +7,11 @@ import {
   Route,
 } from "react-router-dom";
 
-import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-
+// import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
+// import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 function App() {
--- src/components/Carousal.js
+++ src/components/Carousal.js
@@ -4,15 +4,21 @@ export default function Carousal() {
   return (
     <div>
       <div id="carouselExampleFade" className="carousel slide carousel-fade">
-  <div className="carousel-inner">
+  <div className="carousel-inner" id='corousel'>
+    <div className="carousel-caption" style={{zIndex:"10"}}>
+    <form className="d-flex">
+      <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button className="btn btn-outline-success text-white bg-success" type="submit">Search</button>
+    </form>
+    </div>
     <div className="carousel-item active">
-      <img src="https://source.unsplash.com/random/900×700/?burger" className="d-block w-100" style={{filter: "brightness(30%)"}} alt="..."/>
+      <img src="https://source.unsplash.com/random/900x700/?burger" className="d-block w-100" style={{filter: "brightness(30%)"}} alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://source.unsplash.com/random/900×700/?pizza" className="d-block w-100" style={{filter: "brightness(30%)"}}alt="..."/>
+      <img src="https://source.unsplash.com/random/900x700/?pizza" className="d-block w-100" style={{filter: "brightness(30%)"}}alt="..."/>
     </div>
     <div className="carousel-item">
-      <img src="https://source.unsplash.com/random/900×700/?momos" className="d-block w-100" style={{filter: "brightness(30%)"}}alt="..."/>
+      <img src="https://source.unsplash.com/random/900x700/?momos" className="d-block w-100" style={{filter: "brightness(30%)"}}alt="..."/>
     </div>
   </div>
   <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">

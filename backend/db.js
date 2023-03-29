d2e18233afcc8a8f418ac73ebe7601628d620dde
ParthSharma00700 <captainparth007@gmail.com>
2023-03-29 16:24:21 +0530

    Adding signup page

--- backend/db.js
+++ backend/db.js
@@ -1,17 +1,19 @@
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://gofood:Parth123@cluster0.c6dldev.mongodb.net/gofoodmern?retryWrites=true&w=majority'
 
-const mongoDB = async () => {
+const mobgodb = async () => {
   try {
     await mongoose.connect(mongoURI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-
     });
-    console.log("Connected to MongoDB");
+    console.log("Connected to mobgodb");
+    const fetched_data = await mongoose.connection.db.collection("food_items");
+    const data = await fetched_data.find({}).toArray();
+    // console.log(data);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error.message);
+    console.error("Error connecting to mobgodb:", error.message);
   }
 };
 
-module.exports = mongoDB;
+module.exports = mobgodb;

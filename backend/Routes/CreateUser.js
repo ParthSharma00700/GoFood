428aa96ce4cc40bf75fbe9afe556245a35faa0c3
ParthSharma00700 <captainparth007@gmail.com>
2023-03-29 17:43:48 +0530

    login error

--- backend/Routes/CreateUser.js
+++ backend/Routes/CreateUser.js
@@ -3,31 +3,59 @@ const router = express.Router()
 const User = require('../models/User')
 const { body, validationResult } = require('express-validator');
 
-router.post("/createuser" , [
-body('email').isEmail(),
-body('name').isLength({ min: 5 }),
+router.post("/createuser", [
+  body('email').isEmail(),
+  body('name').isLength({ min: 5 }),
   body('password').isLength({ min: 5 })],
-async( req, res) =>{
+  async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    
-    try{
-        await User.create({
-            name:req.body.name,
-            password:req.body.password,
-            email:req.body.email,
-            location:req.body.location
-        })
-        res.json({success:true});
 
-    }catch(error){
-        console.log(error)
-        res.json({success:false});
+    try {
+      await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        location: req.body.location
+      })
+        .then(res.json({ success: true }))
+
+    } catch (error) {
+      console.log(error)
+        .then(res.json({ success: false }));
     }
-})
+  })
+
+
+router.post("/loginuser", [
+  body('email').isEmail(),
+  body('password').isLength({ min: 5 })], async (req, res) => {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    let email = req.body.email;
+    try {
+      let userData = await User.findOne({ email });
+      if (!userData) {
+        return res.status(400).json({ errors: "Try logging with correct credentials" })
+      }
+      if (req.body.password !== userData.password) {
+        return res.status(400).json({ errors: "Try logging with correct credentials" })
+      }
+      return res.json({ success: true })
+
+    } catch (error) {
+      console.log(error)
+        (res.json({ success: false }));
+    }
+  })
+
 
 module.exports = router;
 
